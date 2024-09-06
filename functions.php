816642f615c79367ea6d98c947c9b34fdaaecd1e
Frazer <fgerrymok@gmail.com>
2024-09-06 13:54:16 -0700

    Began to add AOS

--- functions.php
+++ functions.php
@@ -250,3 +250,36 @@ function school_theme_custom_logo_setup() {
 }
 
 add_action('after_setup_theme', 'school_theme_custom_logo_setup');
+
+
+// Enqueue JS scripts
+// function sc_scripts() {
+// 	// AOS Animate on Scroll
+
+// 	wp_enqueue_style(
+// 		'aos-styles',
+// 		get_template_directory_uri() . '/style.css',
+// 		array(),
+// 		'1.0.0',
+// 	);
+
+// 	wp_enqueue_script(
+// 		'aos-script',
+// 		get_template_directory_uri() . '/js/aos.js',
+// 		array(),
+// 		'1.0.0',
+// 		// array( 'strategy' => 'defer' ),
+// 		true,
+// 	);
+
+// 	wp_enqueue_script(
+// 		'theme-js',
+// 		get_template_directory_uri() . '/js/theme.js',
+// 		array('AOS'),
+// 		'1.0.0',
+// 		// array( 'strategy' => 'defer' ),
+// 		true,
+// 	);
+
+// }
+// add_action( 'wp_enqueue_scripts', 'sc_scripts' );
